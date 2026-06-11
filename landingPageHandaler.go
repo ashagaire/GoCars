e840dc8da53dc270fe6a441a1cb5b7c66891d070
ashagaire <asha.gaire95@gmail.com>
2026-06-11 15:35:39 +0300

    added: multiple template files added to render as parent child components in website

--- landingPageHandaler.go
+++ landingPageHandaler.go
@@ -3,10 +3,17 @@ package main
 import (
 	"html/template"
 	"net/http"
+	
 )
 
+
+type carsDataset struct {
+	DummyDataset []TemplateData 
+}
 func landingPageHandaler(w http.ResponseWriter, req *http.Request) {
-	//parsing all template files
+	// showData := []TemplateData {}
+	var data carsDataset
+	//parsing all template files 
 	files := getTemplateFiles()
 	if len(files) == 0 {
 		http.Error(w, "No Template Files Found", http.StatusInternalServerError)
@@ -14,12 +21,12 @@ func landingPageHandaler(w http.ResponseWriter, req *http.Request) {
 	}
 
 	tpl, err1 := template.ParseFiles(files...)
-	if err1 != nil {
+	if  err1!= nil {
 		http.Error(w, err1.Error(), http.StatusInternalServerError)
 		return
 	}
-	data := dataset()
-	err := tpl.ExecuteTemplate(w, "landingPage", data)
+	data.DummyDataset = dataset()
+	err := tpl.ExecuteTemplate(w,"landingPage",data)
 	if err != nil {
 		tpl.ExecuteTemplate(w, err.Error(), data)
 		return
